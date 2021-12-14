9de62bed6450673ab9f7c8a03833299d9e8d3a4c
Timothy Ng <94186690+timothyng0@users.noreply.github.com>
2021-12-14 04:17:09 +0000

    i couldn't completely figure out 2, 3, and 5. 5 is somewhat functional though.

--- part5.py
+++ part5.py
@@ -18,3 +18,13 @@ Enter a number or enter 0 to stop: 0
 Number of evens: 3
 
 '''
+num = int(input("Enter a number or enter 0 to stop:"))
+total = 0
+
+while num != 0:
+  num = int(input("Enter a number or enter 0 to stop:"))
+  if num % 2 == 0:
+    total = total + num
+    num = int(input("Enter a number or enter 0 to stop:"))
+if num == 0:   
+  print("Number of evens: ", total)
